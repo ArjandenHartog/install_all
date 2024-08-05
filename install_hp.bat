4f44d463dd27a4ced4f3bea1f35038ba383316e0
ArjandH <152160720+ArjandenHartog@users.noreply.github.com>
2024-08-05 09:45:47 +0200

    Update install_hp.bat

--- install_hp.bat
+++ install_hp.bat
@@ -1,8 +1,6 @@
 @echo on
-REM Stel het doelpad in waar het installatieprogramma zich bevindt
-set TARGET_DIR="C:\path\to\directory"
 
 REM Voer de stille installatie uit
-"%TARGET_DIR%\1. Pre-installatie\Hp Support Assistent\sp146042.exe" /s /a /s /v"/qn"
+"%TARGET_DIR%\1. Pre-installatie\Hp Support Assistent\sp146042.exe" /a /s /v"/qn"
 
 echo Installatie voltooid.
